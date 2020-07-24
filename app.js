fd20b4590169a864fcf75a2f7b5b07cf2f170f30
Adesh <adesh02092000@gmail.com>
2020-07-24 18:32:28 +0530

    added profile, gitignore and midified port

--- app.js
+++ app.js
@@ -169,6 +169,11 @@ app.post("/submit", function(req, res){
     })
 })
 
-app.listen(3000, function(req, res){
+
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port = 3000;
+}
+app.listen(port, function(req, res){
     console.log("Server spining at port 3000");
 })
